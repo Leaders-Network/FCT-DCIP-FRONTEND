f62199ce43e4dca90e1eab882a19d15eea6cd9d9
Oluwatobiloba <117101891+Intellihackz@users.noreply.github.com>
2024-10-02 14:03:38 +0100

    fix eslint errors

--- src/app/admin/otp/page.tsx
+++ src/app/admin/otp/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import OTPAuthentication from '@/components/admin/OTPAuthentication'
 import Image from 'next/image';
-import React, { useState } from 'react'
+import React from 'react'
 
 const Page = () => {
  
