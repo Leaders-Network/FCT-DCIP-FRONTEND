328ec2283794c6162be8869ffe391d1371fbca7e
ibrahim O. Ibrahim <ibrahimomobolaji1999@gmail.com>
2026-03-25 14:20:53 +0100

    Improve navbar notification icon

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -50,7 +50,7 @@ export default function AdminDashboardLayout({ children }: { children: React.Rea
           isMobile={isMobile}
         />
 
-        <div className="flex-1 flex flex-col overflow-hidden">
+        <div className="flex-1 flex flex-col overflow-visible">
           <Header onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
 
           <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 p-6">
